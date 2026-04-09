78e5d5c329408b05508bfe51d7c4a1ceac5def96
salvagd09 <vasco14_100@hotmail.com>
2026-04-09 11:37:21 -0500

    Login de usuarios

---
+++ NicolasaProyecto/src/IniciarSesion.tsx
@@ -0,0 +1,37 @@
+import { useState } from 'react'
+import './App.css'
+import LoginInicio from './assets/Login_Inicio.png'
+function App() {
+    localStorage.setItem("salvagd09","Cell28")
+    const [username,setUsername]=useState("")
+    const [password,setPassword]=useState("")
+    const submit=(e)=>{
+      e.preventDefault()
+      if(username!==localStorage.key.name || password!=localStorage.key.name){
+          alert("Inicio de sesión exitoso")
+      }else{
+          alert("Inicio de sesión fallido")
+          setUsername("")
+          setPassword("")
+      }
+    }
+  return (
+    <>
+      <section id="center">
+        <h1 className="titulo">Iniciar Sesión</h1>
+        <div className="loginImagen">
+          <img src={LoginInicio} className="base" width="170" height="170" alt="Imagen de inicio de sesión" />
+        </div>
+        <form onSubmit={submit} className="formulario">
+            <label htmlFor="username" className="loginL">Username:</label>
+            <input type="text" id="username" onChange={(e)=>{setUsername(e.target.value)}} className="inputs"/>
+            <label htmlFor="password" className="loginL">Contraseña:</label>
+            <input type="password" id="password" onChange={(e)=>{setPassword(e.target.value)}} className="inputs"/>
+            <button type="submit">Ingresar</button>
+        </form>
+      </section>
+    </>
+  )
+}
+
+export default App
